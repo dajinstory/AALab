daf6ad4d3cc3bc1b0ab46b10fe008b9be6b31d96
dajinstory <dajinstory@naver.com>
2018-10-22 16:35:58 +0900

    audio_augmt

---
+++ audio_augmentation/audio_augmentation/Source.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "audio_augmentation.h"
+#define CLOCKSIZE 44100 * 2 //1초당 data 개수
+
+int main(){
+	int num_of_files = 2;
+	int div = 1; //2초간격 겹치게
+	int time_for_each_files = 3;
+	for (int i = 1; i <= num_of_files; i++){
+		divide_audio(i, time_for_each_files, CLOCKSIZE * div);
+	}
+	return 0;
+}
