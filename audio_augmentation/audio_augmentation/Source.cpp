ae662499896cd8afdc1a0ae5f7a896c45c0eedba
dajinstory <dajinstory@naver.com>
2018-11-16 14:45:11 +0900

    data augmentation

--- audio_augmentation/audio_augmentation/Source.cpp
+++ audio_augmentation/audio_augmentation/Source.cpp
@@ -3,16 +3,11 @@
 #define CLOCKSIZE 44100 * 2 //1초당 data 개수
 
 int main(){
-<<<<<<< HEAD
-	int num_of_files = 20;
-	int div = 1; //1초간격 다른 시작 겹치게
-=======
-	int num_of_files = 1;
-	int div = 1; //2초간격 겹치게
->>>>>>> b95e7cfa5e9f8e0a30d7c5b0c361d58433c6c10c
+	int num_of_files = 50;
+	int div = 5; //0.5초간격 다른 시작 겹치게
 	int time_for_each_files = 3;
 	for (int i = 1; i <= num_of_files; i++){
-		divide_audio(i, time_for_each_files, CLOCKSIZE * div);
+		divide_audio(i, time_for_each_files, CLOCKSIZE * div / 10);
 	}
 	return 0;
 }
--- audio_augmentation/audio_augmentation/audio_augmentation.cpp
+++ audio_augmentation/audio_augmentation/audio_augmentation.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 char name[40];
 short int value[CLOCKSIZE*100];
+short int changed_value[CLOCKSIZE * 100];
 
 void printWAVHeader(wav_header_t header){
 	printf("WAV File Header read:\n");
@@ -25,21 +26,27 @@ void printWAVHeader(wav_header_t header){
 	printf("Sample Rate * Bits/Sample * Channels / 8: %ld\n", header.byteRate);
 	printf("Bits per Sample * Channels / 8.1: %hd\n", header.blockAlign);
 	printf("Bits per Sample: %hd\n", header.bitsPerSample);
+	printf("\n");
 }
 
 void divide_audio(int num, int time, int div){
 	int idx, samples_count = 0, sample_size;
-	FILE *fin, *fout;
+	int cnt = 0;
+	FILE *fin, *headerFile;
 	wav_header_t header;
 	chunk_t chunk;
 	unsigned long chunk_size;
 
 	//load big data
-	sprintf(name, "./input/scream (%d).wav", num);
+	sprintf(name, "./input/5s_nondanger/5s_nondanger (%d).wav", num);
 	fin = fopen(name, "rb");
+	headerFile = fopen("./input/header_format.wav", "rb");
 
 	//load header and chunk
 	fread(&header, sizeof(header), 1, fin);
+	printWAVHeader(header);
+
+	fread(&header, sizeof(header), 1, headerFile);
 	while (true)
 	{
 		fread(&chunk, sizeof(chunk), 1, fin);
@@ -47,7 +54,7 @@ void divide_audio(int num, int time, int div){
 		if (*(unsigned int *)&chunk.ID ==0x61746164)
 			break;
 		if (chunk.ID[2] == 'd' && chunk.ID[3] == 'a'){
-			fseek(fin, -(sizeof(chunk) - 2), SEEK_CUR);
+			fseek(fin, -6, SEEK_CUR);
 			continue;
 		}
 		//skip chunk data bytes
@@ -58,31 +65,37 @@ void divide_audio(int num, int time, int div){
 
 	//load audio data
 	for (idx = 1; feof(fin) == 0; idx++){
-		//load directory to be saved
-		sprintf(name, "./output/data-%d-%d.wav", num, idx);
-		fout = fopen(name, "wb");
-
-		//write header and chunk
-		fwrite(&header, sizeof(header), 1, fout);
-		fwrite(&chunk, sizeof(chunk), 1, fout);
-
 		//read (44100 x time) data
 		for (; samples_count < CLOCKSIZE * time && feof(fin) == 0; samples_count++){
 			fread(&value[samples_count], sample_size, 1, fin);
 		}
-
-		//and write it
-		for (int i = 0; i < samples_count; i++){
-			fwrite(&value[i], sample_size, 1, fout);
+		if (samples_count != CLOCKSIZE*time){
+			break;
 		}
+		for (int volume = -4; volume <= 2; volume ++){
+			//load directory to be saved
+			FILE *fout;
+			sprintf(name, "./output/3s_nondanger/data-%d-%d-(%d).wav", num, idx, volume);
+			fout = fopen(name, "wb");
 
-		//Renew number of samples
-		header.chunkSize = samples_count * 2 + 36;
-		chunk.size = samples_count * 2;
-		fseek(fout, 0, SEEK_SET);
-		fwrite(&header, sizeof(header), 1, fout);
-		fwrite(&chunk, sizeof(chunk), 1, fout);
+			//write header and chunk
+			fwrite(&header, sizeof(header), 1, fout);
+			fwrite(&chunk, sizeof(chunk), 1, fout);
 
+			//write audio data
+			for (int i = 0; i < samples_count; i++){
+				changed_value[i] = value[i] +value[i] / 5 * volume;
+				fwrite(&changed_value[i], sample_size, 1, fout);
+			}
+
+			//Renew number of samples
+			header.chunkSize = samples_count * 2 + 36;
+			chunk.size = samples_count * 2;
+	//		fseek(fout, 0, SEEK_SET);
+	//		fwrite(&header, sizeof(header), 1, fout);
+	//		fwrite(&chunk, sizeof(chunk), 1, fout);
+			fclose(fout);
+		}
 		//move some data for next duplicated things
 		for (int i = 0; i < samples_count - div; i++){
 			value[i] = value[i + div];
@@ -90,7 +103,6 @@ void divide_audio(int num, int time, int div){
 		samples_count -= div;
 	}
 	fclose(fin);
-	fclose(fout);
 	
 }
 
