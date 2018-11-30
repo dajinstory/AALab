89eeb2ae59e2a9c51a259b088c1db70f39df085a
dajinstory <dajinstory@naver.com>
2018-11-30 10:57:44 +0900

    audio_check

---
+++ audio_check/audio_check.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Wav Header
+typedef struct wav_header_t
+{
+	char chunkID[4]; //"RIFF" = 0x46464952
+	unsigned long chunkSize; //28 [+ sizeof(wExtraFormatBytes) + wExtraFormatBytes] + sum(sizeof(chunk.id) + sizeof(chunk.size) + chunk.size)
+	char format[4]; //"WAVE" = 0x45564157
+	char subchunk1ID[4]; //"fmt " = 0x20746D66
+	unsigned long subchunk1Size; //16 [+ sizeof(wExtraFormatBytes) + wExtraFormatBytes]
+	unsigned short audioFormat;
+	unsigned short numChannels;
+	unsigned long sampleRate;
+	unsigned long byteRate;
+	unsigned short blockAlign;
+	unsigned short bitsPerSample;
+	//[WORD wExtraFormatBytes;]
+	//[Extra format bytes]
+}wav_header_t; void printWAVHeader(wav_header_t);
+
+int main(){
+	FILE *fin;
+	wav_header_t header;
+	char name[40];
+	int T = 2;
+
+	for (int i = 0; i < T; i++){
+		scanf(" %s", name);
+		fin = fopen(name, "rb");
+
+		fread(&header, sizeof(header), 1, fin);
+		printWAVHeader(header);
+	}
+
+	return 0;
+}
+
+void printWAVHeader(wav_header_t header){
+	printf("WAV File Header read:\n");
+	printf("File Type: %s\n", header.chunkID);
+	printf("File Size: %ld\n", header.chunkSize);
+	printf("WAV Marker: %s\n", header.format);
+	printf("Format Name: %s\n", header.subchunk1ID);
+	printf("Format Length: %ld\n", header.subchunk1Size);
+	printf("Format Type: %hd\n", header.audioFormat);
+	printf("Number of Channels: %hd\n", header.numChannels);
+	printf("Sample Rate: %ld\n", header.sampleRate);
+	printf("Sample Rate * Bits/Sample * Channels / 8: %ld\n", header.byteRate);
+	printf("Bits per Sample * Channels / 8.1: %hd\n", header.blockAlign);
+	printf("Bits per Sample: %hd\n", header.bitsPerSample);
+	printf("\n");
+}
